81bdf4e5e147d3c9137d1e776a04c9ab99ab2edd
lein-hub <75424893+lein-hub@users.noreply.github.com>
2021-03-09 20:53:37 +0900

    test

--- src/SolutionFileNameSort.java
+++ src/SolutionFileNameSort.java
@@ -4,15 +4,33 @@ public class SolutionFileNameSort {
 	
 	public static void main(String[] args) {
 		String[] files = {"img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"};
+
+		FileName ddd = new FileName("F-5 Freedom Fighter", 0);
+		
+		System.out.println(ddd.getFileName());
+		System.out.println(ddd.getHead());
+		System.out.println(ddd.getNumber());
+		System.out.println(ddd.getTail());
+		
+		for (String a : solution(files))
+			System.out.println(a);
 	}
 	
-	public String[] solution(String[] files) {
-        String[] answer = {};
+	public static String[] solution(String[] files) {
+        String[] answer;
         
         ArrayList<FileName> list = new ArrayList<>();
         
         for (int i=0; i<files.length; i++) {
-        	
+        	list.add(new FileName(files[i], i));
+        }
+        
+        Collections.sort(list);
+        
+        answer = new String[list.size()];
+        
+        for (int i=0; i<list.size(); i++) {
+        	answer[i] = list.get(i).getFileName();
         }
         
         
@@ -25,7 +43,8 @@ class FileName implements Comparable<FileName> {
 
 	private String fileName;
 	private int fileIdx;
-	private String head, number, tail;
+	private String head="", number="", tail="";
+	private byte hnt = 0;
 	
 	
 	
@@ -38,6 +57,7 @@ class FileName implements Comparable<FileName> {
 	}
 
 	public String getHead() {
+		
 		return head;
 	}
 
@@ -49,27 +69,38 @@ class FileName implements Comparable<FileName> {
 		return tail;
 	}
 
-	public FileName(String fileName, int fileIdx, String head, String number, String tail) {
+	public FileName(String fileName, int fileIdx) {
 		this.fileName = fileName;
 		this.fileIdx = fileIdx;
-		this.head = head;
-		this.number = number;
-		this.tail = tail;
+		
+		char[] list = fileName.toCharArray();
+		
+		for (char c : list) {
+			if (!Character.isDigit(c) && hnt == 0) {
+				this.head += c;
+			} else if (Character.isDigit(c) && hnt < 5) {
+				this.number += c;
+				hnt++;
+			} else {
+				this.tail += c;
+				hnt=5;
+			}
+		}
 	}
 	
 	@Override
 	public int compareTo(FileName o) {
 		
-		if (this.getHead().compareTo(o.getHead()) > 0) {
+		if (this.getHead().toLowerCase().compareTo(o.getHead().toLowerCase()) > 0) {
 			return 1;
-		} else if (this.getHead().compareTo(o.getHead()) == 0) {
+		} else if (this.getHead().toLowerCase().compareTo(o.getHead().toLowerCase()) == 0) {
 			if (Integer.parseInt(this.getNumber()) - Integer.parseInt(o.getNumber()) > 0) {
 				return 1;
 			} else if (Integer.parseInt(this.getNumber()) - Integer.parseInt(o.getNumber()) == 0) {
 				return this.getFileIdx() - o.getFileIdx();
 			}
 		}
-		return 0;
+		return -1;
 	}
 	
 }
