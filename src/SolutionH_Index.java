d0625303a61d5169776749fc17ca3dfd3b13b6c5
lein-hub <75424893+lein-hub@users.noreply.github.com>
2021-05-13 21:12:06 +0900

    test

--- src/SolutionH_Index.java
+++ src/SolutionH_Index.java
@@ -1,4 +1,23 @@
 
 public class SolutionH_Index {
-
+	public static void main(String[] args) {
+		
+	}
+	
+	public int solution(int[] citations) {
+        for (int i=citations.length; i>0; i--) {
+        	int breakCount = 0;
+        	int idxStack = 0;
+        	for (int j=0; j<citations.length; j++) {
+        		if (citations[j] < i) {
+        			breakCount++;
+        			if (breakCount > citations.length - i) break;
+        		} else {
+        			idxStack++;
+        			if (idxStack >= i) return i;
+        		}
+        	}
+        }
+        return 0;
+    }
 }
