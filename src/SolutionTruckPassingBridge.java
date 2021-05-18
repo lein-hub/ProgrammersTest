2d0dc2d73c2b1f20fe97c3e48da4bfbe309fbf41
lein-hub <75424893+lein-hub@users.noreply.github.com>
2021-05-18 22:06:14 +0900

    test

---
+++ src/SolutionTruckPassingBridge.java
@@ -0,0 +1,45 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class SolutionTruckPassingBridge {
+	public static void main(String[] args) {
+		int bl = 2;
+		int w = 10;
+		int[] tw = {7,4,5,6};
+		System.out.println(solution(bl, w, tw));
+	}
+	
+	public static int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        Deque<Integer> queue = new ArrayDeque<>();
+        
+       	for (int i = 0; (sum(queue) != 0 || i < truck_weights.length); i++) {
+       		answer++;
+       		if (i >= truck_weights.length) {
+                if (queue.size() >= bridge_length) {
+           			queue.remove();
+                }
+                queue.add(0);
+       		}
+       		else if (queue.size() >= bridge_length) {
+       			queue.remove();
+       		}
+       		else if (sum(queue) + truck_weights[i] <= weight) {
+       			queue.add(truck_weights[i]);
+       		}
+       		else {
+       			queue.add(0);
+       			i--;
+       		}
+       	}
+        return answer;
+    }
+	
+	public static int sum(Deque<Integer> queue) {
+		int result = 0;
+		for (Object i : queue.toArray()) {
+			result += (int) i;
+		}
+		return result;
+	}
+}
